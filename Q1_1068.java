e617415a5d7daf55b727f0dfbf0cde99a9b0b072
Arnabbera119 <148134127+Arnabbera119@users.noreply.github.com>
2023-10-17 23:21:50 +0530

    Add files via upload

---
+++ Q1_1068.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Q1_1068 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        while(n!=1){
+            System.out.print(n+" ");
+            if(n%2==0){
+                n=n/2;
+            }
+            else{
+                n=n*3+1;
+            }
+        }
+        System.out.println(1);
+    }
+}
---
+++ Q3_1069.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q3_1069 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n-1];
+        for(int i=0;i<n-1;i++){
+            arr[i]=sc.nextInt();
+        }
+        long sum=(long)n*((long)n+1)/2,s=0;
+        for(int i=0;i<n-1;i++){
+            s=s+arr[i];
+        }
+        System.out.println(sum-s);
+    }
+}
